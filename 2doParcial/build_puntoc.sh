9c13c34d9ec584ee7ef0ca3b9345cce2cc80e054
German Moltoni <german.moltoni@gmail.com>
2024-11-20 02:30:47 +0000

    feat: creo archivos html en app dir, agrego build script para armar imagen y script puntoc

---
+++ 2doParcial/build_puntoc.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker build -t shermanet/2parcial-ayso:v1.0 .
+docker push shermanet/2parcial-ayso:v1.0
---
+++ 2doParcial/puntoc.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run  -d -p 8080:80 shermanet/2parcial-ayso:v1.0
