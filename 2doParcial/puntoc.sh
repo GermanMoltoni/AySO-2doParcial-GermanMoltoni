58f8d61d1c751a16a10d71374d9f79ba2886632d
German Moltoni <german.moltoni@gmail.com>
2024-11-21 01:47:15 -0300

    verifico que este corriendo el container

--- 2doParcial/puntoc.sh
+++ 2doParcial/puntoc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker run  -d -p 8080:80 shermanet/2parcial-ayso:v1.0
+docker ps
 #Verifico si puedo acceder a las páginas
 #curl http://192.168.56.8:8080/index.html
 #curl http://192.168.56.8:8080/contacto.html
