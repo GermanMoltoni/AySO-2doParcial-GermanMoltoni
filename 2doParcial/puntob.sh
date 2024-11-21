e4428c33171ebdd18a93791471d899f0d85de12f
German Moltoni <german.moltoni@gmail.com>
2024-11-21 01:32:16 -0300

    Update puntob.sh
    Agrego comprobación de instalación de apache en el server de desarrollo

--- 2doParcial/puntob.sh
+++ 2doParcial/puntob.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 ansible-playbook  -i ansible/inventory ansible/playbook.yml
+#Compruebo si esta instalado en el host
+echo 'Ingreso por ssh a host Desarrollo y verifico si se instaló apache'
+ssh vagrant@192.168.56.9 -t "sudo apt list --installed | grep apache"
